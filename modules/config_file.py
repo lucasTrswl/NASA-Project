7b7cd868724838a1e0fc12f8d793d404bddfffca
Elisa Semedo <elisasemedo07@gmail.com>
2024-04-04 10:04:30 +0200

    création d'un fichier de config

--- modules/config_file.py
+++ modules/config_file.py
@@ -1,7 +1,36 @@
-import json
+import configparser
+import os
 
 
-def write_config_file(path_file, data):
-    # data = [1, 2, 3, 4, 5]
-    with open(path_file, "w", encoding="utf-8") as f:
-        json.dump(data, f)
+def write_config_file(
+    path_dir,
+    data,
+):
+    """
+    Crée un fichier de configuration pour un projet
+    Params:
+    - path_dir : chemin du projet sélectionner
+    - data : donnée à mettre dans le fichier de configuration
+    """
+    config = configparser.ConfigParser(allow_no_value=True)
+    config_object = {}
+    for items in data:
+        config_object[items] = None
+    config["DEFAULT"] = {"path": path_dir}
+    config["SELECTED"] = config_object
+    with open(
+        os.path.join(path_dir, "config.ini"), "w", encoding="utf-8"
+    ) as configfile:
+        config.write(configfile)
+
+
+def read_config_file(path_dir):
+    """
+    Récupérer les configurations du projet
+    Params:
+    - path_dir : chemin du projet sélectionner
+    """
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read(os.path.join(path_dir, "config.ini"))
+    all_file = list(config["SELECTED"])
+    return all_file
