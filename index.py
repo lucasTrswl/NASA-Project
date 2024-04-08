cf53971843cf1ed5995f6f3e9bda5b9a72333cb5
Elisa Semedo <elisasemedo07@gmail.com>
2024-04-08 11:06:12 +0200

    merge des 2 pages du menu

--- index.py
+++ index.py
@@ -1,13 +1,10 @@
 import os
 import tkinter as tk
-from tkinter import ttk, filedialog
-from PIL import Image, ImageTk
+from tkinter import ttk, filedialog, messagebox
 from conversion_manager import conversion_manager
 from modules.interface import show_folders, configuration_canvas
 from global_style import (
     couleur_blanc,
-    couleur_bleu,
-    SETTINGS_STYLE,
 )
 
 WIDTH_WINDOW = 800
@@ -17,30 +14,27 @@ def afficher_message():
     print("Bonjour, Tkinter!")
 
 
-def menu_selection(event):
+def menu_selection(event=None):
     """
     Fonction pour gérer la sélection dans le menu déroulant.
 
     Params:
     - event : L'événement de redimensionnement de la fenêtre.
     """
-    global page
-    selected_item = event.widget.get()
-    page = selected_item
+    selected_item = selected_option.get()
     print(f"Menu sélectionné : {selected_item}")
     if selected_item == "Créer un nouveau projet":
         canvas.pack_forget()
         scroll_y.pack_forget()
         scroll_x.pack_forget()
+        clear_frame(scrollable_frame)
         content_frame.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
         champ_label.pack()
         champ_saisie.pack()
         bouton_importer_dossier.pack()
-        clear_frame(frame_projets)
-        # Masquer la Frame des projets
-        frame_projets.pack_forget()
     elif selected_item == "Afficher tous les projets":
         content_frame.pack_forget()
+        # clear_frame(content_frame)
         configuration_canvas(
             canvas=canvas,
             scroll_x=scroll_x,
@@ -61,7 +55,6 @@ def importer_dossier():
         nom_dossier = champ_saisie.get()
         if not nom_dossier:
             nom_dossier = os.path.basename(dossier)
-        liste_projets.append(nom_dossier)
         messagebox.showinfo("Succès", "Dossier importé avec succès")
         print(f"Dossier importé avec succès : {dossier}")
     else:
@@ -84,7 +77,39 @@ fenetre = tk.Tk()
 fenetre.title("Mon Application")
 fenetre.geometry("800x800")
 fenetre.configure(bg="#FFFFFF")
+style = ttk.Style()
 
+style.theme_create(
+    "gendarmerie_style",
+    parent="alt",
+    settings={
+        # "TLabel": {"configure": {"foreground": "#0055A4", "background": "#FFFFFF", "font": ('Helvetica', 12)}},
+        "TEntry": {
+            "configure": {"foreground": "#000000", "font": ("Helvetica", 12)}
+        },
+        # "TButton": {"configure": {"foreground": "#FFFFFF", "background": "#0055A4", "font": ('Helvetica', 12, 'bold')}},
+        "TFrame": {"configure": {"background": "#FFFFFF"}},
+        "TCombobox": {
+            "configure": {
+                "foreground": "#000000",
+                "background": "#FFFFFF",
+                "font": ("Helvetica", 12),
+            }
+        },
+        "TCombobox.Border": {
+            "configure": {"foreground": "#0055A4", "background": "#0055A4"}
+        },
+        "TCombobox.field": {
+            "configure": {
+                "foreground": "#000000",
+                "background": "#FFFFFF",
+                "font": ("Helvetica", 12),
+            }
+        },
+    },
+)
+
+style.theme_use("gendarmerie_style")
 header_frame = ttk.Frame(fenetre)
 header_frame.pack(side=tk.TOP, fill=tk.X)
 
@@ -96,7 +121,7 @@ header_label = ttk.Label(
     foreground="#FFFFFF",
 )
 header_label.pack(padx=10, pady=10, fill=tk.X)
-
+# menu
 menu_options = ["Créer un nouveau projet", "Afficher tous les projets"]
 selected_option = tk.StringVar()
 menu = ttk.Combobox(
@@ -107,17 +132,9 @@ menu = ttk.Combobox(
     font=("Helvetica", 12),
 )
 selected_option.set("Afficher tous les projets")
-menu = ttk.Combobox(
-    header_frame,
-    textvariable=selected_option,
-    values=menu_options,
-    width=15,
-    font=("Helvetica", 12),
-)
 menu.bind("<<ComboboxSelected>>", menu_selection)
 menu.pack(side=tk.LEFT, padx=10, pady=(0, 10))
 
-content_frame = ttk.Frame(fenetre)
 
 # frame pour la liste des dossiers
 canvas = tk.Canvas(
@@ -137,11 +154,8 @@ configuration_canvas(
     scrollable_frame=scrollable_frame,
 )
 
-# show_folders(scrollable_frame, fenetre.winfo_width())
 
-page = "Afficher tous les projets"
-# list_frame.pack(padx=5, pady=5, fill=tk.BOTH, expand=True)
-
+content_frame = ttk.Frame(fenetre)
 champ_label = ttk.Label(
     content_frame, text="Saisissez le nom du dossier", font=("Helvetica", 12)
 )
@@ -150,51 +164,12 @@ bouton_importer_dossier = ttk.Button(
     content_frame, text="Importer le dossier", command=importer_dossier
 )
 
-folder_icon = PhotoImage(file="folder.png").subsample(5)
-
-frame_projets = ttk.Frame(content_frame)
-frame_projets.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-
-canvas_frame = ttk.Frame(canvas)
-canvas.create_window((0, 0), window=canvas_frame, anchor=tk.NW)
-
-
-def on_configure(event):
-    canvas.configure(scrollregion=canvas.bbox("all"))
-
-
-canvas_frame.bind("<Configure>", on_configure)
-
-
-def suivante():
-    global current_index
-    current_index = (current_index + 1) % len(images_photo)
-    afficher_image(current_index)
-
-
-def precedente():
-    global current_index
-    current_index = (current_index - 1) % len(images_photo)
-    afficher_image(current_index)
-
-
-bouton_suivant = ttk.Button(content_frame, text="Suivant", command=suivante)
-bouton_precedent = ttk.Button(
-    content_frame, text="Précédent", command=precedente
-)
-
-bouton_suivant.pack()
-bouton_precedent.pack()
-
-# Gestion de zoom par mouvement de souris
-canvas.bind("<MouseWheel>", zoom)
 
 footer_frame = ttk.Frame(fenetre)
 footer_frame.pack(side=tk.BOTTOM, fill=tk.X)
 
 
-# # dossier = ""
-#
+dossier = ""
 menu_selection()
 
 
@@ -210,7 +185,7 @@ def on_window_resize(event):
     - scrollable_frame : Instance du cadre scrollable de Tkinter. (Frame)
 
     """
-    if page == "Afficher tous les projets":
+    if selected_option.get() == "Afficher tous les projets":
         global previous_width, previous_height
         current_width = fenetre.winfo_width()
         current_height = fenetre.winfo_height()
